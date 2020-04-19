438195561974d8b720df79cf8ff79ceb990d0172
Paweł Szostak <szostak_pawel@outlook.com>
2020-04-19 14:20:38 +0200

    Updated README.md file and deleted unnecessary files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/blog-website",
   siteMetadata: {
     title: `Blog Website`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
